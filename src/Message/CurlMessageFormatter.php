8602b4d388b2a93d051aa81f2c2da8ce93100e5c
Dinesh Kumar Selvamani <dinesh.selvamani@rexsoftware.com.au>
2022-07-12 12:40:26 +1000

    Upgraded guzzle to 7.0 and php to 7.2
    - Updated rtheunissen/guzzle-log-middleware to ^1.0
    - Replaced namshi/cuzzle with rexlabs/cuzzle
    - Updated CurlMessageFormatter to be compatible with more recent guzzle

--- src/Message/CurlMessageFormatter.php
+++ src/Message/CurlMessageFormatter.php
@@ -6,6 +6,7 @@ use GuzzleHttp\MessageFormatter;
 use Namshi\Cuzzle\Formatter\CurlFormatter;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
 /**
  * Class CurlMessageFormatter.
@@ -15,17 +16,17 @@ class CurlMessageFormatter extends MessageFormatter
     /**
      * Returns a formatted message string.
      *
-     * @param RequestInterface  $request  Request that was sent
-     * @param ResponseInterface $response Response that was received
-     * @param \Exception        $error    Exception that was received
+     * @param RequestInterface $request Request that was sent
+     * @param ResponseInterface|null $response Response that was received
+     * @param Throwable|null $error Exception that was received
      *
      * @return string
      */
     public function format(
         RequestInterface $request,
-        ResponseInterface $response = null,
-        \Exception $error = null
-    ) {
+        ?ResponseInterface $response = null,
+        ?Throwable $error = null
+    ): string {
         return $response === null
             ? (new CurlFormatter())->format($request)
             : parent::format($request, $response, $error);
